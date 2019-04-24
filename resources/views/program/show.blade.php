f41a539b713b6274b98505500e8cf94cf7d1100e
stepen victor <stephenvicson@gmail.com>
2019-04-24 21:08:31 +0300

    student experiencs part2 update commit

--- resources/views/program/show.blade.php
+++ resources/views/program/show.blade.php
@@ -27,7 +27,7 @@
         <div class="medium-9 small-12 columns posts-wrap">
 
             <div class="event-thumb">
-                <img src="{{ asset('images/event_thumb.jpg') }}" alt="Something0" class="thumbnail" />
+                <img src="{{ asset('images/event_thumb.jpg') }}"  class="thumbnail" />
             </div><!-- Course Thumb /-->
 
 
