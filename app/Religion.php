92d18026f74d7a13b7bff27df60b2add58336d10
kyalos <30431524+kyalos@users.noreply.github.com>
2019-05-24 12:45:36 +0300

    Application form dummy. removed loops because of errors

---
+++ app/Religion.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Resizable;
+
+class Religion extends Model
+{
+
+	use Resizable;
+	//
+    protected $guarded = [];
+}
