9d970a39d95ee8d5229d23896ae9519188da3b08
stepen victor <stephenvicson@gmail.com>
2019-07-03 13:24:53 +0300

    worked on the governace and footer list.

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,6 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage/applications',
             'visibility' => 'public',
         ],
 
